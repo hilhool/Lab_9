3cde4dd4a5b48abc841845b0f84f6f1acfda04e8
hilhool <timur45433@gmail.com>
2025-01-20 22:03:53 +0500

    Добавьте файлы проекта.

---
+++ Dish.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Lab_9
+{
+    internal class Dish
+    {
+        public double proteins;
+        public double fats;
+        public double carbohydrations;
+        public double calories;
+        public double Calories()
+        {
+           double result = 4 * proteins + 9 * fats + 4 * carbohydrations;
+            return result;
+        }
+        public Dish(double proteins, double fats, double carbohydrations)
+        {
+            this.proteins = proteins;
+            this.fats = fats;
+            this.carbohydrations = carbohydrations;
+
+            calories = Calories();
+        }
+        public Dish()
+        {
+            calories = 0; 
+        }
+        public void Print()
+        {
+            Console.WriteLine($"при {proteins} белка, {fats} жиров, {carbohydrations} углеводах каллорийность равна: ");
+            Console.Write(calories);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dish dish1 = new Dish(54, 65, 76);
+            dish1.Calories();
+            dish1.Print();
+        }
+    }
+}
