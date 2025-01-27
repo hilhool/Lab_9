16f95f752818fe4caf3db3395d00e6b80b1c1fdb
hilhool <timur45433@gmail.com>
2025-01-27 18:42:49 +0500

    feat: fixed

--- Dish.cs
+++ Dish.cs
@@ -9,31 +9,107 @@ namespace Lab_9
 {
     internal class Dish
     {
-        public double proteins;
-        public double fats;
-        public double carbohydrations;
-        public double calories;
-        public double Calories()
+        private double proteins;
+        private double fats;
+        private double carbohydrations;
+        private double calories;
+        private static int count = 0;
+
+#region свойства
+        public double Proteins
         {
-           double result = 4 * proteins + 9 * fats + 4 * carbohydrations;
-            return result;
+            get
+            {
+                return proteins;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Белки не могут быть отрицательными");
+                else
+                    proteins = value;
+            }
         }
-        public Dish(double proteins, double fats, double carbohydrations)
+        public double Fats
+        {
+            get
+            {
+                return fats;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("жиры не могут быть отрицательными");
+                else
+                    fats = value;
+            }
+        }
+        public double Carbohydrations
+        {
+            get
+            {
+                return carbohydrations;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("углеводы не могут быть отрицательными");
+                else
+                    carbohydrations = value;
+            }
+        }
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        #endregion свойства
+
+#region конструкторы
+        public Dish(double proteins, double fats, double carbohydrations) //Конструктор с параметром 
         {
             this.proteins = proteins;
             this.fats = fats;
             this.carbohydrations = carbohydrations;
 
             calories = Calories();
+            count++;
         }
-        public Dish()
+        public Dish()  //Конструктор без параметра 
         {
-            calories = 0; 
+            proteins = 0;
+            fats = 0;
+            carbohydrations = 0;
+
+            calories = 0;
+            count++;
+        }
+        public Dish(Dish defaultDish)  //Конструктор копирования 
+        {
+            this.proteins = defaultDish.proteins;
+            this.fats = defaultDish.fats;
+            this.carbohydrations = defaultDish.carbohydrations;
+
+            calories = Calories();
+            count++;
+        }
+        #endregion конструкторы
+
+#region методы
+        public double Calories()
+        {
+            double result = Math.Round(4 * proteins + 9 * fats + 4 * carbohydrations, 2);
+            return result;
+        }
+        public static double Calories(Dish dish)
+        {
+            return Math.Round(4 * dish.proteins + 9 * dish.fats + 4 * dish.carbohydrations, 2);
         }
         public void Print()
         {
-            Console.WriteLine($"при {proteins} белка, {fats} жиров, {carbohydrations} углеводах каллорийность равна: ");
-            Console.Write(calories);
+            Console.Write($"при {proteins} белка, {fats} жиров, {carbohydrations} углеводах каллорийность равна ");
+            Console.WriteLine(calories);
         }
+        #endregion методы
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿namespace Lab_9
+﻿using System;
+
+namespace Lab_9
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Dish dish1 = new Dish(54, 65, 76);
-            dish1.Calories();
             dish1.Print();
+
+            Dish dish2 = new Dish(dish1); // Использование конструктора копирования
+            dish2.Print();
+
+            Console.WriteLine($"Общее количество созданных объектов: {Dish.GetCount()}");
+
+            double calories = dish1.Calories();
+            Console.WriteLine($"Калорийность рассчитанная статическим методом: {calories} ккал");
         }
-    }
+    } 
 }
